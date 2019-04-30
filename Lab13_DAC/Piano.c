e8ec8217a7693ac4b53706c7c01b548fcf7d4491
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2019-04-30 14:33:39 -0700

    Implement Piano_In()

--- Lab13_DAC/Piano.c
+++ Lab13_DAC/Piano.c
@@ -33,6 +33,5 @@ void Piano_Init(void){
 // 0x01 is key 0 pressed, 0x02 is key 1 pressed,
 // 0x04 is key 2 pressed, 0x08 is key 3 pressed
 unsigned long Piano_In(void){
-  
-  return 0; // remove this, replace with input
+  return GPIO_PORTE_DATA_R&0xF;
 }
