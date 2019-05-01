f00b6bf68c419774949192122a0fec093f6c6a22
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2019-05-01 15:01:48 -0700

    Generate tone based switch input

--- Lab13_DAC/Lab13.c
+++ Lab13_DAC/Lab13.c
@@ -19,16 +19,34 @@ void delay(unsigned long msec);
 int main(void){ // Real Lab13 
 	// for the real board grader to work 
 	// you must connect PD3 to your DAC output
+  Piano_Key input, previous;
   TExaS_Init(SW_PIN_PE3210, DAC_PIN_PB3210,ScopeOn); // activate grader and set system clock to 80 MHz
 // PortE used for piano keys, PortB used for DAC        
   Sound_Init(); // initialize SysTick timer and DAC
   Piano_Init();
   EnableInterrupts();  // enable after all initialization are done
+  previous = (Piano_Key) Piano_In();
   while(1){                
 // input from keys to select tone
-
+    input = (Piano_Key) Piano_In();
+    if (input == C_Key && (previous != input)) {        // key 0x01 just pressed
+      Sound_Play(C_Note);
+    }
+    else if (input == D_Key && (previous != input)) {   // key 0x02 just pressed
+      Sound_Play(D_Note);
+    }
+    else if (input == E_Key && (previous != input)) {   // key 0x04 just pressed
+      Sound_Play(E_Note);
+    }
+    else if (input == G_Key && (previous != input)) {   // key 0x08 just pressed
+      Sound_Play(G_Note);
+    }
+    else if (previous && (input == 0)) {       // key just released
+      Sound_Off();
+    }
+    previous = input;
+    delay(10);      // debounce switch
   }
-            
 }
 
 // Inputs: Number of msec to delay
@@ -43,5 +61,3 @@ void delay(unsigned long msec){
     msec--;
   }
 }
-
-
