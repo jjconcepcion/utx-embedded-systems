4dccd8f4fd419d124ccf436659a568d0c90d4606
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2019-04-30 18:53:25 -0700

    Set initial SysTick reload to zero
    We want the Sound_Tone() to set reload and start SysTick ISR

--- Lab13_DAC/Sound.c
+++ Lab13_DAC/Sound.c
@@ -27,7 +27,7 @@ void Sound_Init(void){
   DAC_Init();
   Index = 0;
   NVIC_ST_CTRL_R &= ~0x1;   // disable SysTick during setup
-  NVIC_ST_RELOAD_R = 0x00FFFFFF;  //initial reload value
+  NVIC_ST_RELOAD_R = 0;     // initial reload value
   NVIC_ST_CURRENT_R = 0;    // write any value to clear current
   NVIC_ST_CTRL_R = 0x7;     // enable SysTick with system clk src and interrups
 }
