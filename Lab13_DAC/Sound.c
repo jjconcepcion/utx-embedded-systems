87d3a327b7760d640032be2594963b2f73537a15
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2019-05-01 16:54:55 -0700

    Adjust SineWave shape to pass real board grader

--- Lab13_DAC/Sound.c
+++ Lab13_DAC/Sound.c
@@ -16,7 +16,7 @@ const unsigned long BUS_CLK_FREQ = 80000000;    // system clock 80MHz
 // 4-bit 32 element sine wave
 const unsigned long N_WAVE_ELEM = 32;
 const unsigned char SineWave[N_WAVE_ELEM] =
-{8,9,10,11,12,13,14,15,15,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,1,1,2,3,4,5,6,7};
+{8,9,11,12,13,14,14,15,15,15,14,14,13,12,11,9,8,7,5,4,3,2,2,1,1,1,2,2,3,4,5,7};
 unsigned char Index;    // index varies from 0 to 31
 
 // **************Sound_Init*********************
